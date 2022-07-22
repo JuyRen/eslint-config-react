6eaeeae03556153d1625335235f92aedec88f4e3
juyren <juyren@163.com>
2022-07-22 11:19:36 +0800

    chore:

---
+++ example/index.ts
@@ -0,0 +1,3 @@
+import { ReactNode } from "react";
+
+import { age, Name } from "./type";
---
+++ example/type.ts
@@ -0,0 +1,11 @@
+export type Name = "lucy" | "jack";
+
+const date = "1";
+
+export const age = 1;
+
+interface Person {
+    name: Name;
+}
+
+export { Person };
