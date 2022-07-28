3b821214f9396182a20b06a15c54cc74d1883a05
juyren <juyren@163.com>
2022-07-28 13:58:50 +0800

    feat: 添加type imports规则

---
+++ example/auto-type.ts
@@ -0,0 +1,4 @@
+import type { Name } from "./constant";
+import { name, Person } from "./constant";
+
+const a: Name = "jon";
---
+++ example/constant.ts
@@ -0,0 +1,10 @@
+export const name = "lucy";
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+export type Name = "jon" | "juy";
+
+export { Person };
